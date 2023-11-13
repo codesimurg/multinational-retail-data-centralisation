6cba1c9005b8cdc441a2a1f36228ea84a99edbdd
codesimurg <codesimurg@gmail.com>
2023-11-13 21:16:02 +0000

    updated files

--- data_extraction.py
+++ data_extraction.py
@@ -1,2 +1,10 @@
 
-class DataExtractor
+import pandas as pd
+
+class DataExtractor:
+    def read_rds_table(self, db_connector, table_name):
+        engine = db_connector.init_db_engine()
+        query = f"SELECT * FROM {table_name}"
+        df = pd.read_sql(query, engine)
+        return df
+    
--- database_utils.py
+++ database_utils.py
@@ -1,2 +1,28 @@
 
 class DatabaseConnector
+import yaml
+from sqlalchemy import create_engine
+from sqlalchemy import inspect
+
+class DatabaseConnector:
+    
+    def read_db_creds(self):
+        with open('db_creds.yaml', 'r') as file:
+            db_creds = yaml.safe_load(file)
+        return db_creds
+    
+    def init_db_engine(self):
+        db_creds = self.read_db_creds()
+        connection_string = f"postgresql://{db_creds['RDS_USER']}:{db_creds['RDS_PASSWORD']}@{db_creds['RDS_HOST']}:{db_creds['RDS_PORT']}/{db_creds['RDS_DATABASE']}"
+        engine = create_engine(connection_string)
+        return engine
+    
+    def list_db_tables(self):
+        engine = self.init_db_engine()
+        inspector = inspect(engine)
+        table_names = inspector.get_table_names()
+        return table_names
+    
+    
+
+    
